9f3b80f50267bf498ffab87cc88ec211987afbef
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2022-12-28 20:38:42 +0500

    added the xml format coverage reported generation flow

--- test/test-apimatic-core/test_helper.rb
+++ test/test-apimatic-core/test_helper.rb
@@ -1,4 +1,8 @@
 require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
+                                                                SimpleCov::Formatter::CoberturaFormatter])
 SimpleCov.start do
   add_filter 'test'
   enable_coverage :branch
