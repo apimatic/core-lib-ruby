db02d489d7113e0239496eae0a4483be577ec730
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-02-26 18:57:55 +0500

    feat(http-header): add support for complex types
    This commits adds support for processing complex object-typed header parameters in the request builder class. Also, adds the case-insensitivity behavior in the headers which was previously missing. Also, adds unit tests to validate handling of complex header parameters
    
    Closes #58

--- test/test-apimatic-core/request_builder_test.rb
+++ test/test-apimatic-core/request_builder_test.rb
@@ -160,10 +160,18 @@ class RequestBuilderTest < Minitest::Test
 
   def test_header_param
     actual = MockHelper.create_basic_request_builder
-                       .header_param(MockHelper.new_parameter("value", key: "key"))
+                       .header_param(MockHelper.new_parameter("value", key: "string"))
+                       .header_param(MockHelper.new_parameter(10, key: "number"))
+                       .header_param(MockHelper.new_parameter(MockHelper.get_person_model, key: "model"))
+                       .header_param(MockHelper.new_parameter([1, 2, 3, 4], key: "array"))
+                       .header_param(MockHelper.new_parameter({ key1: 'value1', key2: 'value2' }, key: "hash"))
                        .build({})
 
-    assert(actual.headers["key"] == "value")
+    assert(actual.headers["string"] == "value")
+    assert(actual.headers["model"] == CoreLibrary::ApiHelper.json_serialize(MockHelper.get_person_model))
+    assert(actual.headers["number"] == '10')
+    assert(actual.headers["array"] == '[1,2,3,4]')
+    assert(actual.headers["hash"] == '{"key1":"value1","key2":"value2"}')
   end
 
   def test_form_param
@@ -290,8 +298,17 @@ class RequestBuilderTest < Minitest::Test
     actual = MockHelper.create_basic_request_builder_with_global_headers
                        .build({})
 
-    assert(actual.headers[:globalHeader] == "value")
-    assert(actual.headers[:additionalHeader] == "value")
+    assert(actual.headers[:globalHeaderString] == "value")
+    assert(actual.headers[:globalHeaderModel] == CoreLibrary::ApiHelper.json_serialize(MockHelper.get_person_model))
+    assert(actual.headers[:globalHeaderNumber] == '20')
+    assert(actual.headers[:globalHeaderArray] == '[1,2,3,4]')
+    assert(actual.headers[:globalHeaderHash] == '{"key1":"value1","key2":"value2"}')
+
+    assert(actual.headers[:additionalHeaderString] == "value")
+    assert(actual.headers[:additionalHeaderModel] == CoreLibrary::ApiHelper.json_serialize(MockHelper.get_person_model))
+    assert(actual.headers[:additionalHeaderNumber] == '20')
+    assert(actual.headers[:additionalHeaderArray] == '[1,2,3,4]')
+    assert(actual.headers[:additionalHeaderHash] == '{"key1":"value1","key2":"value2"}')
   end
 
   def test_multipart_param
