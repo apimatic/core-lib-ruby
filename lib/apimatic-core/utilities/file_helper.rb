ef2717556c87d42158a2afe4c3f7015ff1a9ed88
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2025-04-04 10:58:38 +0500

    fix(file-handling): file not being closed after opening and caching (#61)
    This commit fixes the file writing to the temp directory which is then cached. The file is created but is not being closed in the flow while getting the file in file helper class.
    
    Closes #60

--- lib/apimatic-core/utilities/file_helper.rb
+++ lib/apimatic-core/utilities/file_helper.rb
@@ -1,21 +1,27 @@
 require 'tempfile'
 require 'open-uri'
+
 module CoreLibrary
-  # A utility for file specific operations.
+  # A utility for file-specific operations.
   class FileHelper
     @cache = {}
 
-    # Class method which takes a URL, downloads the file (if not already downloaded.
-    # for this test session) and returns the path of the file.
+    # Class method which takes a URL, downloads the file (if not already downloaded
+    # for this test session), and returns the file path.
     # @param [String] url The URL of the required file.
+    # @return [String] The path of the downloaded file.
     def self.get_file(url)
-      unless @cache.keys.include? url
-        @cache[url] = Tempfile.new('APIMatic')
-        @cache[url].binmode
-        @cache[url].write(URI.parse(url).open({ ssl_ca_cert: Certifi.where }).read)
-      end
-      @cache[url].rewind
-      @cache[url].path
+      return @cache[url] if @cache.key?(url)
+
+      tempfile = Tempfile.new('APIMatic')
+      tempfile.binmode
+      tempfile.write(URI.parse(url).open(ssl_ca_cert: Certifi.where).read)
+      tempfile.flush
+      tempfile.close
+
+      raise 'Tempfile path is nil!' if tempfile.path.nil?
+
+      @cache[url] = tempfile.path.to_s # Store only the file path
     end
   end
 end
