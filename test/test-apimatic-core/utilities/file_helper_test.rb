f110547851edaaf0d44ba6378d9102dd798a18eb
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2025-04-08 14:19:54 +0500

    fix(file-handling): add file rewind and update cache type (#62)
    This commit adds the file rewinding and also updates the file cache type as the cache map now stores the Tempfile type which will be returned as is if available. Previously, the file was being closed and was being deleted by garbage collector due to the temporary file.

--- test/test-apimatic-core/utilities/file_helper_test.rb
+++ test/test-apimatic-core/utilities/file_helper_test.rb
@@ -21,4 +21,16 @@ class FileHelperTest < Minitest::Test
     end
 
   end
+
+  def test_get_same_file
+    file_url = 'https://gist.githubusercontent.com/asadali214/' \
+      '0a64efec5353d351818475f928c50767/raw/8ad3533799ecb4e01a753aaf04d248e6702d4947/testFile.txt'
+    expected_file_content = 'This test file is created to test CoreFileWrapper functionality'
+
+    File::open(FileHelper.get_file(file_url)) do |actual_file|
+      refute_nil actual_file
+      assert_equal expected_file_content.encode('ascii'), actual_file.read()
+    end
+
+  end
 end
