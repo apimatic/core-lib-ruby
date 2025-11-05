afd0933592781633cabb0f09fd8e31412e9390f9
MaryamAdnan <maryam.adnan@apimatic.io>
2025-11-05 15:42:10 +0500

    added from_hash implementation of Proxy_Settings

--- lib/apimatic-core/http/configurations/proxy_settings.rb
+++ lib/apimatic-core/http/configurations/proxy_settings.rb
@@ -38,5 +38,25 @@ module CoreLibrary
         hash[:password] = password if password
       end
     end
+
+    ##
+    # Creates a ProxySettings instance from a hash.
+    #
+    # @param hash [Hash] A hash containing :address, :port, :username, and/or :password.
+    # @return [ProxySettings, nil] Returns a new instance or nil if hash is nil/empty.
+    #
+    def self.from_hash(hash)
+      return nil if hash.nil? || hash.empty?
+
+      # Support both symbol and string keys
+      address = hash[:address] || hash['address']
+      port = hash[:port] || hash['port']
+      username = hash[:username] || hash['username']
+      password = hash[:password] || hash['password']
+
+      return nil if address.nil? || address.strip.empty?
+
+      new(address: address, port: port, username: username, password: password)
+    end
   end
 end
